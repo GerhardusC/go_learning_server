5d1c20a7d697563938a76b3e6de148d0e6d58f07
Gerhardus <hardus.cronje@gmail.com>
2025-05-21 23:35:05 +0200

    feat: updated rust core with consistency and logs

--- dataCollection/rust_core/src/main.rs
+++ dataCollection/rust_core/src/main.rs
@@ -1,7 +1,7 @@
 use chrono::Local;
 use clap::Parser;
 use rusqlite::{params, Connection};
-use std::time::Duration;
+use std::{io::{Error, ErrorKind}, time::Duration};
 
 use color_eyre::Result;
 use mosquitto_rs::*;
@@ -21,26 +21,60 @@ struct Cli {
     broker_ip: String,
 }
 
+enum SensorValue {
+    NUM(f32),
+    STR(String)
+}
+
 struct SensorReading {
     timestamp: u64,
     topic: String,
-    value: String,
+    value: SensorValue,
 }
 
 impl SensorReading {
     fn new(timestamp: u64, topic: String, value: String) -> SensorReading {
+        let parsed_val = if let Ok(float) = value.parse::<f32>() {
+            SensorValue::NUM(float)
+        } else {
+            SensorValue::STR(value)
+        };
         SensorReading {
             timestamp,
             topic,
-            value,
+            value: parsed_val,
         }
     }
+
     fn add_to_db(&self, args: &Cli) -> Result<()> {
         let connection = Connection::open(&args.db_path)?;
-        connection.execute(
-            "INSERT INTO READINGS VALUES (?1, ?2, ?3);",
-            params![self.timestamp, self.topic, self.value],
-        )?;
+        match &self.value {
+            SensorValue::NUM(float_val) => {
+                connection.execute(
+                    "INSERT INTO MEASUREMENTS VALUES (?1, ?2, ?3);",
+                    params![self.timestamp, self.topic, float_val],
+                )?;
+                println!(
+                    "\x1b[1;30;104m{}:\x1b[0m\x1b[1m {}\x1b[0m saved to \x1b[1;30;46mMEASUREMENTS\x1b[0m at {}",
+                    self.topic,
+                    float_val,
+                    self.timestamp
+                )
+            },
+            SensorValue::STR(string_val) => {
+                connection.execute(
+                    "INSERT INTO LOGS VALUES (?1, ?2, ?3);",
+                    params![self.timestamp, self.topic, string_val],
+                )?;
+                println!(
+                    "\x1b[1;30;104m{}:\x1b[0m\x1b[1m {}\x1b[0m saved to \x1b[1;30;103mLOGS\x1b[0m at {}",
+                    self.topic,
+                    string_val,
+                    self.timestamp
+                )
+            },
+        }
+
         Ok(())
     }
 }
@@ -52,10 +86,20 @@ async fn main() -> Result<()> {
 
     let connection = Connection::open(&args.db_path)?;
     connection.execute("
-        CREATE TABLE if not exists READINGS (
-            timestamp int NOT NULL,
-            topic varchar(255) NOT NULL,
-            value float(2) NOT NULL
+        CREATE TABLE if not exists MEASUREMENTS (
+                timestamp int,
+                topic varchar(255),
+                value float
+        )
+        ",
+        (),
+    )?;
+
+    connection.execute("
+        CREATE TABLE if not exists LOGS (
+                timestamp int,
+                topic varchar(255),
+                value float
         )
         ",
         (),
@@ -87,17 +131,50 @@ async fn subscribe(args: &Cli) -> Result<()> {
     client.subscribe(topic, QoS::AtMostOnce).await?;
 
     loop {
-        if let Some(subs) = &subscriptions {
-            if let Ok(msg) = subs.recv().await {
-                if let Event::Message(message) = msg {
-                    let reading = SensorReading::new(
-                        Local::now().timestamp().try_into()?,
-                        message.topic,
-                        String::from_utf8(message.payload)?,
-                    );
-                    reading.add_to_db(&args)?;
-                }
+        if let Some(sub) = &subscriptions {
+            match sub.recv().await {
+                Ok(msg) => {
+                    respond_to_event(msg, args)?;
+                },
+                Err(err) => {
+                    println!("Error receiveing: {}", err)
+                },
             }
+        } else {
+                println!("Error with event")
         }
     }
+}
+
+fn respond_to_event (msg: Event, args: &Cli) -> Result<()> {
+    match msg {
+        Event::Message(message) => {
+            let now_timestamp_i64 = Local::now().timestamp();
+            let now = if let Ok(val) = u64::try_from(now_timestamp_i64) {
+                val
+            } else {
+                println!("Failed to convert u64 to i64");
+                now_timestamp_i64 as u64
+            };
+            let reading = SensorReading::new(
+                now,
+                message.topic,
+                String::from_utf8(message.payload)?,
+            );
+
+            reading.add_to_db(&args)?;
+        },
+        Event::Connected(connection_status) => {
+            println!("\x1b[1;30;42mConnected:\x1b[0m {}", connection_status)
+        },
+        Event::Disconnected(reason_code) => {
+            println!("\x1b[1;30;41mDisconnected:\x1b[0m {}", reason_code);
+            return Err(
+                Error::new(
+                    ErrorKind::Other, "Something went wrong, disconnected."
+                ).into()
+            )
+        },
+    }
+    Ok(())
 }
