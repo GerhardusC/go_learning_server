1e3fc160563766697763941d2e32ad882e8ba514
Gerhardus <hardus.cronje@gmail.com>
2025-05-22 21:31:10 +0200

    feat: cleaned up bg functions

--- dataCollection/rust_core/src/data_collection.rs
+++ dataCollection/rust_core/src/data_collection.rs
@@ -1,4 +1,8 @@
 use crate::ARGS;
+use redis::Commands;
+use chrono::Local;
+use std::time::Instant;
+use redis;
 use rusqlite::{params, Connection};
 use color_eyre::Result;
 
@@ -14,7 +18,7 @@ pub struct MosquittoMessage {
 }
 
 impl MosquittoMessage {
-    pub fn new(timestamp: u64, topic: String, value: String) -> MosquittoMessage {
+    fn new(timestamp: u64, topic: String, value: String) -> MosquittoMessage {
         let parsed_val = if let Ok(float) = value.parse::<f32>() {
             SensorValue::NUM(float)
         } else {
@@ -27,7 +31,7 @@ impl MosquittoMessage {
         }
     }
 
-    pub fn add_to_db(&self) -> Result<()> {
+    fn add_to_db(&self) -> Result<()> {
         let connection = Connection::open(&ARGS.db_path)?;
         match &self.value {
             SensorValue::NUM(float_val) => {
@@ -58,5 +62,64 @@ impl MosquittoMessage {
 
         Ok(())
     }
+}
+
+fn save_value_to_redis (topic: String, msg: String) -> Result<()> {
+    let performance_now = Instant::now();
+    let redis_client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut connection = redis_client.get_connection()?;
+    let res = connection.set::<&str, &str, String>(&topic, &msg);
+    match res {
+        Ok(_) => {
+            println!(
+                "\x1b[1;30;104m{}:\x1b[0m \x1b[1m{}\x1b[0m added in \x1b[1;30;104mREDIS\x1b[0m at {}",
+                topic,
+                msg,
+                Local::now().timestamp()
+            );
+            println!(
+                "\x1b[1;30;47mSave to \x1b[0m\x1b[1;30;106mREDIS\x1b[0m duration: \x1b[1;96m{}\x1b[0m us",
+                &performance_now.elapsed().as_micros()
+            )
+        },
+        Err(err) => {
+            println!("\x1b[1;30;41mError:\x1b[0m Something went wrong while adding data to redis: {}", err)
+        },
+    }
+    Ok(())
+}
+
+pub fn save_value_to_redis_bg (topic: String, msg: String) {
+    tokio::spawn(async move {
+        match save_value_to_redis(topic, msg) {
+            Ok(_) => (),
+            Err(err) => {
+                    println!("\x1b[1;30;41mError:\x1b[0m Something went wrong while connecting to redis: {}", err)
+            },
+        }
+    });
+}
+
+pub fn save_value_to_db_bg (topic: String, msg: String) {
+    tokio::spawn(async move {
+        let performance_now = Instant::now();
+        let now = Local::now().timestamp();
+        let reading = MosquittoMessage::new(
+            now.try_into().unwrap_or(now as u64),
+            topic,
+            msg,
+        );
+        match reading.add_to_db() {
+            Ok(_) => {
+                println!(
+                    "\x1b[1;30;47mSave to \x1b[0m\x1b[1;30;45mDB\x1b[0m duration: \x1b[1;35m{}\x1b[0m us",
+                    &performance_now.elapsed().as_micros()
+                )
+            },
+            Err(err) => {
+                println!("Something went wrong while adding data to the db: {}", err)
+            },
+        }
+    });
 }
 
--- dataCollection/rust_core/src/event_handling.rs
+++ dataCollection/rust_core/src/event_handling.rs
@@ -1,12 +1,9 @@
 use crate::ARGS;
-use crate::data_collection::MosquittoMessage;
-use redis::Commands;
-use chrono::Local;
-use std::{io::{Error, ErrorKind}, time::{Duration, Instant}};
+use crate::data_collection::{save_value_to_db_bg, save_value_to_redis_bg};
+use std::{io::{Error, ErrorKind}, time::Duration};
 use mosquitto_rs::*;
 use tokio::time::sleep;
 use color_eyre::Result;
-use redis;
 
 pub async fn start_subscription_loop () {
     loop {
@@ -22,8 +19,6 @@ pub async fn start_subscription_loop () {
 }
 
 async fn subscribe_to_base_topic() -> Result<()> {
-    let redis_client = redis::Client::open("redis://127.0.0.1/")?;
-
     let mosquitto_client = Client::with_auto_id()?;
     mosquitto_client
         .connect(&ARGS.broker_ip, 1883, Duration::from_secs(5), None)
@@ -40,7 +35,7 @@ async fn subscribe_to_base_topic() -> Result<()> {
         if let Some(sub) = &subscriptions {
             match sub.recv().await {
                 Ok(msg) => {
-                    respond_to_event(msg, &redis_client)?;
+                    respond_to_event(msg)?;
                 },
                 Err(err) => {
                     println!("Error receiveing: {}", err)
@@ -52,70 +47,13 @@ async fn subscribe_to_base_topic() -> Result<()> {
     }
 }
 
-fn respond_to_event (msg: Event, redis_client: &redis::Client) -> Result<()> {
+fn respond_to_event (msg: Event) -> Result<()> {
     match msg {
         Event::Message(message) => {
             let msg_str = String::from_utf8(message.payload)?;
 
-            let now_timestamp_i64 = Local::now().timestamp();
-            let now = if let Ok(val) = u64::try_from(now_timestamp_i64) {
-                val
-            } else {
-                println!("Failed to convert u64 to i64");
-                now_timestamp_i64 as u64
-            };
-            let reading = MosquittoMessage::new(
-                now,
-                (&message.topic).to_owned(),
-                (&msg_str).to_owned(),
-            );
-
-            let performance_now = Instant::now();
-            tokio::spawn(async move {
-                match reading.add_to_db() {
-                    Ok(_) => {
-                        println!(
-                            "\x1b[1;30;47mSave to \x1b[0m\x1b[1;30;45mDB\x1b[0m duration: \x1b[1;35m{}\x1b[0m us",
-                            &performance_now.elapsed().as_micros()
-                        )
-                    },
-                    Err(err) => {
-                        println!("Something went wrong while adding data to the db: {}", err)
-                    },
-                }
-            });
-
-            let performance_now = Instant::now();
-            let connection = redis_client.get_connection();
-
-            match connection {
-                Ok(mut conn) => {
-                    tokio::spawn(async move {
-                        let res = conn.set::<&str, &str, String>(&message.topic, &msg_str);
-                        match res {
-                            Ok(_) => {
-                                println!(
-                                    "\x1b[1;30;104m{}:\x1b[0m \x1b[1m{}\x1b[0m added in \x1b[1;30;104mREDIS\x1b[0m at {}",
-                                    message.topic,
-                                    msg_str,
-                                    now
-                                );
-                                println!(
-                                    "\x1b[1;30;47mSave to \x1b[0m\x1b[1;30;106mREDIS\x1b[0m duration: \x1b[1;96m{}\x1b[0m us",
-                                    &performance_now.elapsed().as_micros()
-                                )
-                            },
-                            Err(err) => {
-                                println!("\x1b[1;30;41mError:\x1b[0m Something went wrong while adding data to redis: {}", err)
-                            },
-                        }
-                    });
-                },
-                Err(err) => {
-                        println!("\x1b[1;30;41mError:\x1b[0m Something went wrong while connecting to redis: {}", err)
-                },
-            }
-
+            save_value_to_db_bg(message.topic.clone(), msg_str.clone());
+            save_value_to_redis_bg(message.topic, msg_str);
         },
         Event::Connected(connection_status) => {
             println!("\x1b[1;30;42mConnected:\x1b[0m {}", connection_status)
@@ -131,3 +69,4 @@ fn respond_to_event (msg: Event, redis_client: &redis::Client) -> Result<()> {
     }
     Ok(())
 }
+
